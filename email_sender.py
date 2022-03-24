c63efd00a8753afcc7822d108f82b35bf62c1e54
KronosGR <geo.elgeo@gmail.com>
2022-03-24 08:35:21 +0100

    sent email

--- email_sender.py
+++ email_sender.py
@@ -1,5 +1,17 @@
 import smtplib                            # creates smtp server
 from email.message import EmailMessage
 
+email = EmailMessage()
+email['from']= 'Kronos'
+email['to'] = 'geo.elgeo@gmail.com'
+email['subject'] = 'You won 10000000 dollars'
 
+email.set_content('Learning python')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('geo.elgeo@gmail.com', '123456')
+  smtp.send_message(email)
+  print('Email was sent')
 
