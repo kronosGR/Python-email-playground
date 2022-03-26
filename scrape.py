f3cf0f214f67ba70b4b49e50cb5e51c0900fc02e
KronosGR <geo.elgeo@gmail.com>
2022-03-26 16:46:24 +0100

    get the links ant the votes

--- scrape.py
+++ scrape.py
@@ -2,4 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+
+links = soup.select('.storylink')
+votes = soup.select('.score')
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', none)
+        points = int(votes[idx].getText().replace(' points', ''))
+        hn.append({'title': title, 'link': href})
+    return hn
+
+
+print(create_custom_hn(links, votes))
